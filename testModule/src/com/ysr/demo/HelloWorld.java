1dccefcefcea61d90ae3b483a9f5f19c7efb0465
yangshizhou2 <yangshizhou@meiya.com>
2018-03-21 16:14:13 +0800

    Initial commit

---
+++ testModule/src/com/ysr/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ysr.demo;
+
+/**
+ * Created by Administrator on 2018/3/21.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("ni hoao");
+    }
+}
